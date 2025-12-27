866a00bd6bced01cf097f33ee720b3072d8f3bc2
govardhan <govardhanbiyyala@gmail.com>
2025-12-27 15:29:20 +0530

    Create Constructor2.java

---
+++ Module1_Array/Constructor2.java
@@ -0,0 +1,24 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Student{
+    String name;
+    int age;
+    
+    public Student(String a,int b){
+     this.name=a;
+     this.age=b;
+    }
+}
+
+
+
+class Main {
+    public static void main(String[] args) {
+       Student a= new Student("govardhan",20);
+       System.out.println(a);
+       System.out.println(a.name);
+       System.out.println(a.age);
+        
+    }
+}
